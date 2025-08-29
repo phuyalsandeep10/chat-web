31e9efa126266bd12afd3e0ef2b206787dd2983b
Deepesh Sunuwar <deepgeneral33@gmail.com>
2025-08-29 10:44:51 +0545

    widget background fixed

--- public/widget.js
+++ public/widget.js
@@ -7,7 +7,13 @@
   iframe.src = 'http://localhost:3000/chat?orgId=' + orgId;
   iframe.style.width = '100%';
   iframe.style.height = '100vh';
+  iframe.style.background = 'transparent';
+  iframe.style.zIndex = 99;
+  iframe.style.position = 'fixed';
+  iframe.style.bottom = '0px';
+  iframe.style.right = '0px';
   iframe.style.border = '0';
   iframe.style.borderWidth = '0';
+  iframe.id = 'chatboq-widget';
   document.body.appendChild(iframe);
 })();
