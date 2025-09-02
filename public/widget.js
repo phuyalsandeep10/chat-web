2a857b1422d76167eff435e45e2cab8fce08de6a
Satis9815 <satiskumarchaudhary52@gmail.com>
2025-09-02 13:13:23 +0545

    fix:Live Url Changed in widget

--- public/widget.js
+++ public/widget.js
@@ -4,7 +4,7 @@
   console.log('Loaded widget for org:', orgId);
 
   const iframe = document.createElement('iframe');
-  iframe.src = 'http://localhost:3000/chat?orgId=' + orgId;
+  iframe.src = 'https://portal.chatboq.com/chat?orgId=' + orgId;
   iframe.style.width = '100%';
   iframe.style.height = '100vh';
   iframe.style.background = 'transparent';
